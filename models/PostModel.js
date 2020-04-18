05a6d78426373e9835bddf359e2365386e592246
mdee <design4web2014@gmail.com>
2020-04-18 12:21:26 +0430

    adding the post model

---
+++ models/PostModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  text: {
+    type: String,
+    required: 'Name is required'
+  },
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
+  likes: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+  comments: [
+    {
+      text: String,
+      created: { type: Date, default: Date.now },
+      postedBy: { type: mongoose.Schema.ObjectId, ref: 'User' }
+    }
+  ],
+  postedBy: { type: mongoose.Schema.ObjectId, ref: 'User' },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model('Post', postSchema, 'posts');
+
+module.exports = Post;
