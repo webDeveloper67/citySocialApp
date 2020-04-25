2e1588a30d3dd782f59554b8a331e94610f07572
mdee <design4web2014@gmail.com>
2020-04-25 18:16:23 +0430

    redux library is up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+// REDUX
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import ReduxToastr from 'react-redux-toastr';
+
 const App = () => {
-  return (
-    <div className="App">
-      <h5>App</h5>
-    </div>
-  );
+  return <Provider store={store} />;
 };
 
 export default App;
---
+++ client/src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducer';
+
+const middleware = [thunk];
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
