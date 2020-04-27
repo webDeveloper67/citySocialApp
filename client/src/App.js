929080c109e53050b3d3bb495955c7ec083b41c2
mdee <design4web2014@gmail.com>
2020-04-27 17:47:12 +0430

    adding Menu View component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// MUI
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { indigo, amber } from '@material-ui/core/colors';
+
+// Layout
+import Navbar from './Features/Layout/Navbar';
 
 // REDUX
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import ReduxToastr from 'react-redux-toastr';
 
+// Color Palette
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6573c3',
+      main: '#3f51b5',
+      dark: '#2c387e',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ffcf33',
+      main: '#ffc400',
+      dark: '#b28900',
+      contrastText: '#000'
+    },
+    openTitle: indigo['400'],
+    protectedTitle: amber['400'],
+    type: 'light'
+  },
+  textDecNone: {
+    textDecoration: 'none'
+  }
+});
+
 const App = () => {
-  return <Provider store={store} />;
+  return (
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+        </Router>
+      </MuiThemeProvider>
+    </Provider>
+  );
 };
 
 export default App;
