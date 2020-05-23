40e09d879b63501eff9a77958f8265ddd966fd1e
mdee <design4web2014@gmail.com>
2020-05-23 16:49:18 +0430

    adding FollowGrid component

--- client/src/Features/Profile/FollowGrid.js
+++ client/src/Features/Profile/FollowGrid.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
-const FollowGrid = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 2,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    background: theme.palette.background.paper
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+    margin: 'auto'
+  },
+  gridList: {
+    width: 500,
+    height: 220
+  },
+  tileText: {
+    textAlign: 'center',
+    marginTop: 10
+  }
+}));
+
+const FollowGrid = ({ people }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h6>follow Grid</h6>
+    <div className={classes.root}>
+      <GridList cellHeight={160} className={classes.gridList} cols={4}>
+        {people.map((person, i) => {
+          return (
+            <GridListTile style={{ height: 120 }} key={i}>
+              <Link to={'/user/' + person._id}>
+                <Avatar
+                  src={'/api/v1/users/photo/' + person._id}
+                  className={classes.bigAvatar}
+                />
+                <Typography className={classes.tileText}>
+                  {person.name}
+                </Typography>
+              </Link>
+            </GridListTile>
+          );
+        })}
+      </GridList>
     </div>
   );
 };
